cc31b8a6da95ba8c4d9839c39be2c6208c27cbf5
Davidson Dias <davidsonsd16@gmail.com>
2019-10-29 17:29:07 -0300

    Interface

--- Taxpayer_AbstractMethod_Csharp/Program.cs
+++ Taxpayer_AbstractMethod_Csharp/Program.cs
@@ -1,18 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using Taxpayer_AbstractMethod_Csharp.Entities;
 
 namespace Taxpayer_AbstractMethod_Csharp {
     class Program {
         static void Main(string[] args) {
             List<Taxpayer> list = new List<Taxpayer>();
-            list.Add(new Individual("Alex", 50000.00, 2000.00));
-            list.Add(new Company("Alex", 50000.00, 20));
 
-            foreach(Taxpayer taxpayer in list) {
-                Console.WriteLine(taxpayer);
+            Console.Write("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"Tax payer #{i} data:");
+                Console.Write("Individual or company (i/c)? ");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                String name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double income = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (type == 'i') {
+                    Console.Write("Health expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Individual(name, income, healthExpenditures));
+                }
+                else {
+                    Console.Write("Number of employees: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+                    list.Add(new Company(name, income, numberOfEmployees));
+                }
             }
-            
+
+            double sum = 0.0;
+            Console.WriteLine();
+            Console.WriteLine("TAXES PAID:");
+            foreach (Taxpayer taxpayer in list) {
+                double tax = taxpayer.Tax();
+                Console.WriteLine(taxpayer);
+                sum += tax;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("TOTAL TAXES: $ " + sum.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
+
 }
